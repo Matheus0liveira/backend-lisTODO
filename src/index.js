b7b9617d3f49b5584b30fa0a6a5666b375cd0dc8
Matheus0liveira <matheusfilho98.ms@gmail.com>
2020-12-08 13:31:54 -0300

    feat: altered port

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import routes from './routes';
 
 import './database';
 
+const PORT = process.env.PORT || 3333;
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () => console.log('🔥 Server started at: http://localhost:3333'));
+app.listen(PORT, () => console.log('🔥 Server started at: http://localhost:3333'));
--- src/routes.js
+++ src/routes.js
@@ -10,17 +10,14 @@ const router = Router();
 // Session route
 router.post('/users/signin', SessionController.store);
 
-
 // User route
 router.post('/users/signup', UserControllers.store);
 router.get('/users', UserControllers.index);
-
 
 // Tasks routes
 router.get('/users/:user_id/tasks', authMiddleware, TasksController.index);
 router.post('/users/:user_id/tasks', authMiddleware, TasksController.store);
 router.delete('/users/:user_id/tasks/:task_id', authMiddleware, TasksController.delete);
-
 
 // Checked route
 router.get('/users/:user_id/checktasks', authMiddleware, CheckedController.index);
