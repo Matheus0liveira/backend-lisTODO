9097c5f189c0b5d0672a9cdd608db23d149eb16b
Matheus0liveira <matheusfilho98.ms@gmail.com>
2020-12-08 22:14:57 -0300

    feat: update cors

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3333;
 
 const app = express();
 app.use(cors({
-  origin: 'https://listtodoo.netlify.app/*',
+  origin: 'https://listtodoo.netlify.app',
 
 }));
 app.use(express.json());
